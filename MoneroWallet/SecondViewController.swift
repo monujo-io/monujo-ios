7ea80b1524c1bee03f97ea1d47b8e950d6ddcff5
Ugo Bataillard <ugo@bataillard.me>
2016-07-14 15:51:27 +0900

    Initial Commit

---
+++ MoneroWallet/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MoneroWallet
+//
+//  Created by Ugo on 7/14/16.
+//  Copyright © 2016 Ugo Bataillard. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
